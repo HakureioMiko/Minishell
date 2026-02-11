b048a2f53617a3630fda054ded133b54c935f92f
mickzhan <mickzhan@student.42.fr>
2026-02-11 11:52:27 +0100

    commencement erreur parsing debut fin pour les token operateur, et doublon cote a cote

--- minishell/src/parsing.c
+++ minishell/src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 15:05:38 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/02/10 18:57:19 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/02/11 11:50:38 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,47 @@ void	rendu_args(t_token *token, char **cmd)
 	}
 }
 
-// geten -> "token"
+// Si "$var" geten -> "token"
+// Si $var -> cree une fonction qui prend chaque separer d'un espace
+// Si '$var' juste afficher $var
+
+bool	lst_last_token(t_token *token)
+{
+	while (token->next != NULL)
+		token = token->next;
+	if (token->type != 0)
+		return (true);
+	return (false);
+}
+
+bool	last_next_token(t_token *token)
+{
+	int	i;
+
+	i = 0;
+	while (token->next != NULL)
+	{
+		if (token->type != 0 && token->next->type != 0)
+			return (true);
+		token = token->next;
+	}
+	return (false);
+}
+
+bool	check_token(t_token *token)
+{
+	if (token->type != 0 || lst_last_token(token) == 1
+		|| last_next_token(token) == 1)
+		return (true);
+	return (false);
+}
+
 int	parser(t_token *token)
 {
-    printf("%s", getenv(token->sub_token->var));
+	if (check_token(token) == 1)
+	{
+		ft_printf(2, "PRINTF TESTER : syntax error near unexpected token\n");
+		return (ERROR_SYNTAX_STATUS);
+	}
 	return (0);
 }
