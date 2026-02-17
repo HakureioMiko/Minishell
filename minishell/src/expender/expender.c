4c9975e8dbdb40a1561d490a2f5dfdf684c923f5
mickzhan <mickzhan@student.42.fr>
2026-02-17 15:29:08 +0100

    originale expender 17 fevrier.

--- minishell/src/expender/expender.c
+++ minishell/src/expender/expender.c
@@ -6,55 +6,130 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:29:35 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/02/17 11:47:48 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/02/17 15:24:12 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-
 // copy permet de copie la valeur de $
-// paste permet de la mettre en leurs valeur et le renvoye 
+// paste permet de la mettre en leurs valeur et le renvoye
 
-typedef struct s_exp
-{
-	int				last_command;
+// typedef struct s_exp
+// {
+// 	int				last_command;
 
-	char			*copy;
-	char			*paste;
-	t_state			state;
-	t_env			*env;
-	bool			in_doc;
-	struct s_exp	*next;
-}					t_exp;
+// 	char			*copy;
+// 	char			*paste;
+// 	t_state			state;
+// 	t_env			*env;
+// 	bool			in_doc;
+// 	struct s_exp	*next;
+// }					t_exp;
 
-bool	check_left_exp(t_ast *ast)
+bool	check_if_word(t_ast *ast)
 {
 	if (ast->type == AST_WORD)
 		return (true);
 	return (false);
 }
 
-bool	check_right_exp(t_ast *ast)
+bool	check_if_expendable(char *ast)
 {
-	if (ast->type == AST_WORD)
-		return (true);
+	int	i;
+
+	i = 0;
+	while (ast[i])
+	{
+		if (ast[i] == '$')
+			return (true);
+		i++;
+	}
 	return (false);
+}
+
+bool	check_path2(char *ast, t_env *env)
+{
+
+}
+
+bool	check_path(char *ast, t_env *env)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	str = ft_calloc(1, (ft_strlen(ast) + 1));
+	if (!str)
+		return (NULL);
+	while (ast[i])
+	{
+		if (ast[i] == '$')
+		{
+			i++;
+			while (ast[i])
+			{
+				str[j] = str[i];
+				i++;
+			}
+		}
+		i++;
+	}
+	i = check_path2(str, env);
+	free(str);
+	return (i);
+}
+
+char	*app_expend(char *ast, t_env *env)
+{
+	if (check_if_expendable(ast) == 0)
+		return (ast);
+	else if (check_path(ast, env) == 0)
+		return (NULL);
+	else
+	{
+	}
+}
+
+t_ast	*call_expand(t_ast *ast, t_env *env)
+{
+	while (ast->cmd_token != NULL)
+	{
+		if (ast->cmd_token->sub_token->quote != SINGLE)
+			ast->cmd_token->sub_token->var = app_expend(ast->cmd_token->sub_token->var,
+					env);
+		ast->cmd_token = ast->cmd_token->next;
+	}
+	return (ast);
+}
+
+t_ast	*expand_left(t_ast *curseurl, t_env *env)
+{
+	while (curseurl->type != AST_WORD)
+	{
+		curseurl = curseurl->left;
+		if (check_if_word(curseurl) == 1)
+			call_expand(curseurl, env);
+		else
+			expand_left(curseurl->right, env);
+		if (check_if_word(curseurl) == 1)
+			call_expand(curseurl, env);
+	}
 }
 
 t_ast	*expand_function(t_ast *ast, t_env *env)
 {
-	t_ast	*curseur;
-	t_exp	*exp;
+	t_ast	*curseurl;
+	t_ast	*curseurr;
 
-	exp = malloc(sizeof(t_exp));
-	curseur = ast;
-	if (curseur->type != AST_WORD)
+	curseurl = ast;
+	curseurr = ast;
+	expand_left(curseurl, env);
+	if (curseurr->type != AST_WORD)
 	{
-		if (curseur->left)
-			check_left_exp(ast);
-		if (curseur->right)
-			check_right_exp(ast);
+		if (curseurr->left && check_if_word(curseurr) == true)
 	}
 	return (ast);
 }
