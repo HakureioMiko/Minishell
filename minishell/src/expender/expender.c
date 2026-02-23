9d4385f8f446f7a6309140b2487957b1d5243163
mickzhan <mickzhan@student.42.fr>
2026-02-23 15:36:58 +0100

    ajout prototype de l'expender

--- minishell/src/expender/expender.c
+++ minishell/src/expender/expender.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expender.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibrouin- <ibrouin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:29:35 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/02/20 14:12:20 by ibrouin-         ###   ########.fr       */
+/*   Updated: 2026/02/23 15:35:15 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,119 +27,290 @@
 // 	struct s_exp	*next;
 // }					t_exp;
 
-// bool	check_if_word(t_ast *ast)
-// {
-// 	if (ast->type == AST_CMD)
-// 		return (true);
-// 	return (false);
-// }
+bool	check_if_word(t_ast *ast)
+{
+	if (ast->type == AST_CMD)
+		return (true);
+	return (false);
+}
 
-// bool	check_if_expendable(char *ast)
+bool	check_if_expendable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+// int	len_dollars(char *ast)
 // {
 // 	int	i;
 
 // 	i = 0;
 // 	while (ast[i])
 // 	{
-// 		if (ast[i] == '$' && ast[i + 1] >= 33 && ast[i + 1] <= 126)
-// 			return (true);
+// 		if (ast[i] == ' ' || ast[i] == '$')
+// 			return (i);
 // 		i++;
 // 	}
-// 	return (false);
+// 	return (i);
 // }
 
-// bool	check_path2(char *ast, t_env *env)
-// {
-// 	int	i;
+char	*check_string(char *str, t_env *env)
+{
+	char	*str_env;
 
-// 	i = 0;
-// 	while (env != NULL)
-// 	{
-// 		i = 0;
-// 		while (env->key[i] == ast[i])
-// 		{
-// 			if (ast[i] == '\0')
-// 				return (false);
-// 			i++;
-// 		}
-// 		env == env->next;
-// 	}
-// 	return (true);
-// }
+	if (!str)
+		return (NULL);
+	str_env = NULL;
+	while (env != NULL)
+	{
+		if (ft_strncmp(env->key, str, -1) == 0)
+		{
+			str_env = ft_strdup(env->content);
+			break ;
+		}
+		env = env->next;
+	}
+	return (str_env);
+}
 
-// bool	check_path(char *ast, t_env *env)
+char	*check_key(char *str)
+{
+	int		i;
+	char	*key;
+
+	i = 0;
+	while ((str[i] != ' ' && str[i]) && (str[i] != '$' && str[i]))
+		i++;
+	key = malloc(sizeof(char) * (i + 1));
+	if (!key)
+		return (NULL);
+	i = 0;
+	while ((str[i] != ' ' && str[i]) && (str[i] != '$' && str[i]))
+	{
+		key[i] = str[i];
+		i++;
+	}
+	key[i] = '\0';
+	return (key);
+}
+
+char	*check_new_string(char *str, char *key, char *env)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		len;
+	int		count;
+	char	*new_string;
+
+	i = 0;
+	k = 0;
+	count = 0;
+	len = ft_strlen(str) + ft_strlen(env) - ft_strlen(key) - 1;
+	new_string = malloc(sizeof(char) * (len + 1));
+	if (!new_string)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == '$' && count == 0 && env != NULL)
+		{
+			i++;
+			j = 0;
+			i += ft_strlen(key);
+			count++;
+			while (env[j])
+			{
+				new_string[k] = env[j];
+				j++;
+				k++;
+			}
+		}
+		else if (env == NULL && str[i] == '$' && count == 0)
+			i += ft_strlen(key) + 1;
+		new_string[k] = str[i];
+		i++;
+		k++;
+	}
+	new_string[k] = '\0';
+	free(str);
+	return (new_string);
+}
+
+// char	*new_string(char *str, t_env *env)
 // {
-// 	char	*str;
 // 	int		i;
-// 	int		j;
+// 	char	*str_key;
+// 	char	*str_env;
+// 	char	*new_str;
+// 	char	*temp;
 
 // 	i = 0;
-// 	j = 0;
-// 	str = ft_calloc(1, (ft_strlen(ast) + 1));
 // 	if (!str)
 // 		return (NULL);
-// 	while (ast[i])
+// 	new_str = ft_strdup(str);
+// 	if (!new_str)
+// 		return (NULL);
+// 	while (new_str[i])
 // 	{
-// 		if (ast[i] == '$')
+//         printf("CONTENT CHAR: %c\n", new_str[i]);
+// 		if (new_str[i] == '$')
 // 		{
 // 			i++;
-// 			while (ast[i])
+//             printf("NEW_STRING AVANT : %s\n", new_str);
+// 			str_key = check_key(new_str + i);
+// 			str_env = check_string(str_key, env);
+// 			if (str_env)
 // 			{
-// 				str[j] = str[i];
-// 				i++;
+// 				temp = check_new_string(new_str, str_key, str_env);
+// 				new_str = temp;
+//                 i = 0;
 // 			}
+// 			else
+// 				i += ft_strlen(str_key) + 1;
+// 			free(str_key);
+// 			free(str_env);
 // 		}
-// 		i++;
+// 		else
+// 			i++;
 // 	}
-// 	i = check_path2(str, env);
-// 	return (free(str), i);
+//     printf("NEW_STRING APRES : %s\n", new_str);
+// 	free(str);
+// 	return (new_str);
 // }
 
-// char	*app_expend(char *ast, t_env *env)
-// {
-// 	if (check_if_expendable(ast) == 0)
-// 		return (ast);
-// 	else if (check_path(ast, env) == 0)
-// 		return (NULL);
-// 	else
-// 	{
-// 	}
-// }
+char	*new_string(char *str, t_env *env)
+{
+	int		i;
+	char	*key;
+	char	*content;
+	char	*new_str;
+	char	*tmp;
 
-// // changer a partir de a ne pas changer var mais mettre dans un tableau
+	i = 0;
+	new_str = ft_strdup(str);
+	while (new_str[i])
+	{
+		if (new_str[i] == '$')
+		{
+			i++;
+			key = check_key(new_str + i);
+			content = check_string(key, env);
+			tmp = check_new_string(new_str, key, content);
+			new_str = tmp;
+			i = 0;
+		}
+		i++;
+	}
+	free(str);
+	return (new_str);
+}
 
+char	*app_expend(char *ast, t_env *env, bool state)
+{
+	if (!ast)
+		return (NULL);
+	if (check_if_expendable(ast) == 0)
+		return (ast);
+	else
+	{
+		if (state == true)
+			ast = new_string(ast, env);
+		else if (state == false)
+			ast = new_string(ast, env);
+	}
+	return (ast);
+}
+
+// changer a partir de a ne pas changer var mais mettre dans un tableau
+
+t_ast	*call_expand(t_ast *ast, t_env *env)
+{
+	t_token		*current_token;
+	t_sub_token	*current_sub;
+	char		**tmp;
+	int			i;
+	int			j;
+
+	ast->cmd = malloc(sizeof(char *) * 100);
+	i = 0;
+	j = 0;
+	current_token = ast->cmd_token;
+	while (current_token != NULL && current_token->type == WORD)
+	{
+		current_sub = current_token->sub_token;
+		while (current_sub != NULL)
+		{
+			if (current_sub->quote == DOUBLE)
+			{
+				current_sub->var = app_expend(current_sub->var, env, true);
+				ast->cmd[i] = ft_strdup(current_sub->var);
+				i++;
+			}
+			else if (current_sub->quote == NORMAL)
+			{
+				current_sub->var = app_expend(current_sub->var, env, false);
+				tmp = ft_split(current_sub->var, ' ');
+				while (tmp[j])
+				{
+					ast->cmd[i] = ft_strdup(tmp[j]);
+					j++;
+					i++;
+				}
+			}
+			j = 0;
+			current_sub = current_sub->next;
+		}
+		current_token = current_token->next;
+	}
+	ast->cmd[i] = NULL;
+	i = 0;
+	while (ast->cmd[i])
+	{
+		printf("%s\n", ast->cmd[i]);
+		i++;
+	}
+	return (ast);
+}
+
+// call expend de la version cmd
 // t_ast	*call_expand(t_ast *ast, t_env *env)
 // {
-// 	while (ast->cmd_token != NULL && ast->cmd_token->type == WORD)
-// 	{
-// 		ast->cmd_token = ast->cmd_token->next;
-// 	}
+// 	t_sub_token	*current_sub;
+
+// 	current_sub = ast->cmd_token->sub_token;
+// 	if (current_sub->quote == DOUBLE)
+// 		current_sub->var = app_expend(current_sub->var, env, true);
+// 	else if (current_sub->quote == NORMAL)
+// 		current_sub->var = app_expend(current_sub->var, env, false);
 // 	return (ast);
 // }
 
-// t_ast	*expand_left(t_ast *curseurl, t_env *env)
-// {
-// 	while (curseurl->type != AST_CMD)
-// 	{
-// 		curseurl = curseurl->left;
-// 		if (check_if_word(curseurl) == 1)
-// 			call_expand(curseurl, env);
-// 		else
-// 			expand_left(curseurl->right, env);
-// 		if (check_if_word(curseurl) == 1)
-// 			call_expand(curseurl, env);
-// 	}
-// }
+t_ast	*expand_ast_checker(t_ast *curseur, t_env *env)
+{
+	if (!curseur)
+		return (NULL);
+	if (check_if_word(curseur) == 1)
+		call_expand(curseur, env);
+	if (curseur->left)
+		expand_ast_checker(curseur->left, env);
+	if (curseur->right)
+		expand_ast_checker(curseur->right, env);
+	return (curseur);
+}
 
-// t_ast	*expand_function(t_ast *ast, t_env *env)
-// {
-// 	t_ast	*curseurl;
-// 	t_ast	*curseurr;
+t_ast	*expand_function(t_ast *ast, t_env *env)
+{
+	t_ast	*curseur;
 
-// 	curseurl = ast;
-// 	curseurr = ast;
-// 	expand_left(curseurl, env);
-// 	// if (curseurr->type != AST_WORD)
-// 	// 	if (curseurr->left && check_if_word(curseurr) == true)
-// 	return (ast);
-// }
+	curseur = ast;
+	ast = expand_ast_checker(curseur, env);
+	return (ast);
+}
